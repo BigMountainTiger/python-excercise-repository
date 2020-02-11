0fd44a89ae6f59c39309ef0cd4c3ce997ff3f3f2
Song Li <da_tou_li@yahoo.com>
2020-02-11 15:47:54 -0500

    Changed the comments

--- a_01_oo_excercise/p_01_an_empty_class.py
+++ a_01_oo_excercise/p_01_an_empty_class.py
@@ -12,6 +12,6 @@ obj_1.attribute2 = "The attribute 2"
 print(obj_1.attribute1 + ' - ' + obj_1.attribute2)
 print(obj_1.__dict__)
 
-# An attribute can be deleted from an object
+# Attributes can be deleted from an object
 del obj_1.attribute2
 print(obj_1.__dict__)
