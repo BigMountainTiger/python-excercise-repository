bec448b302173f06891611c835cb8ccf502b8db2
Song Li <da_tou_li@yahoo.com>
2020-02-11 15:59:59 -0500

    Added the file p_02_constructor_instance_method.p

---
+++ a_01_oo_excercise/p_02_constructor_instance_method.py
@@ -0,0 +1,16 @@
+# A class with an instructor and an instance method
+class AClass:
+  def __init__(self, attr_1, attr_2):
+    self.attr_1 = attr_1
+    self.attr_2 = attr_2
+
+  def print_attributes(self):
+    print(self.attr_1 + ' - ' + self.attr_2)
+
+# Create two instances
+obj_1 = AClass('obj_1_a1', 'obj_1_a2')
+obj_2 = AClass('obj_2_a1', 'obj_2_a2')
+
+# Call the instance method
+obj_1.print_attributes()
+obj_2.print_attributes()
