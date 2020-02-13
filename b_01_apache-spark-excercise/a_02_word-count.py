0b10f8eacf0247d208d70cf661e00fd87abb118b
Song Li <da_tou_li@yahoo.com>
2020-02-13 16:54:57 -0500

    palying with it

--- b_01_apache-spark-excercise/a_02_word-count.py
+++ b_01_apache-spark-excercise/a_02_word-count.py
@@ -7,7 +7,7 @@ if __name__ == "__main__":
     sc = SparkContext(conf = conf)
     sc.setLogLevel('ERROR')
     
-    rdd = sc.parallelize(['This', 'is', 'is', 'cool'], 2)
+    rdd = sc.parallelize(('This', 'is', 'is', 'cool'), 2)
     
     print('No of partitions - {}'.format(rdd.getNumPartitions()))
     wordCounts = rdd.countByValue()
