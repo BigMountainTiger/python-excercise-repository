025f6f67c50857ae4ccdd7cabbe716a5b125654e
Song Li <da_tou_li@yahoo.com>
2020-02-13 17:11:34 -0500

    Playing with variable renaming

--- b_01_apache-spark-excercise/a_02_word-count.py
+++ b_01_apache-spark-excercise/a_02_word-count.py
@@ -4,11 +4,11 @@ if __name__ == "__main__":
     conf = SparkConf().setAppName("word count") \
       .setMaster("local[2]")
 
-    sc = SparkContext(conf = conf)
-    sc.setLogLevel('ERROR')
+    context = SparkContext(conf = conf)
+    context.setLogLevel('ERROR')
     
     data = ('This', 'is', 'is', 'cool')
-    rdd = sc.parallelize(data, 2)
+    rdd = context.parallelize(data, 2)
     
     print('No of partitions - {}'.format(rdd.getNumPartitions()))
     wordCounts = rdd.countByValue()
