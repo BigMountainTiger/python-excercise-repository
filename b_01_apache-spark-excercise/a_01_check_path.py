32cf64c5006630c6d6c3a1f371c2f47bfe028828
Song Li <da_tou_li@yahoo.com>
2020-02-13 16:33:45 -0500

    Added the first spark example

--- b_01_apache-spark-excercise/a_01_check_path.py
+++ b_01_apache-spark-excercise/a_01_check_path.py
@@ -1,7 +1,8 @@
-import sys, inspect
+import sys, inspect, pyspark
 
 print('Python package path:')
 for path in sys.path:
   print(path)
 
 print('Inspect sys - {}'.format(inspect.getmodule(sys)))
+print('Inspect pyspark - {}'.format(inspect.getmodule(pyspark)))
---
+++ b_01_apache-spark-excercise/a_02_word-count.py
@@ -0,0 +1,15 @@
+from pyspark import SparkContext, SparkConf
+
+if __name__ == "__main__":
+    conf = SparkConf().setAppName("word count") \
+      .setMaster("local[2]")
+
+    sc = SparkContext(conf = conf)
+    sc.setLogLevel('ERROR')
+    
+    rdd = sc.parallelize(['This', 'is', 'is', 'cool'], 2)
+    
+    print('No of partitions - {}'.format(rdd.getNumPartitions()))
+    wordCounts = rdd.countByValue()
+    for word, count in wordCounts.items():
+        print("{} : {}".format(word, count))
