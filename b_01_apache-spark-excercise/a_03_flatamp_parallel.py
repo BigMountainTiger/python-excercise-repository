44e7e7949c3f3b379d69e07d37e69abc9d81798e
Song Li <da_tou_li@yahoo.com>
2020-02-14 16:54:06 -0500

    Played with loading data from files

--- b_01_apache-spark-excercise/a_03_flatamp_parallel.py
+++ b_01_apache-spark-excercise/a_03_flatamp_parallel.py
@@ -1,8 +1,11 @@
 from pyspark import SparkContext, SparkConf
 import os
 
-def getAList(id):
-  return ['This', 'is', 'Item-{}-{}'.format(id, os.getpid())]
+def getAListFromAFile(file_path):
+  lines = open(file_path).read().splitlines()
+  lines.append('From process ({})'.format(os.getpid()))
+
+  return lines
 
 def a_03_flatamp_parallel():
   conf = SparkConf().setAppName("word count") \
@@ -11,18 +14,22 @@ def a_03_flatamp_parallel():
   context = SparkContext(conf = conf)
   context.setLogLevel('ERROR')
   
-  rdd = context.parallelize([1, 2], 2) \
-    .flatMap(lambda id: getAList(id)).cache()
+  dir = '/home/song/sandbox/python-excercise-repository/' \
+    + 'b_01_apache-spark-excercise/test-files/a_03/{}'
+  path_1 = dir.format('file-1.txt')
+  path_2 = dir.format('file-2.txt')
+
+  file_paths = [path_1, path_2]
+  rdd = context.parallelize(file_paths, 2) \
+    .flatMap(lambda id: getAListFromAFile(id)).cache()
   
-  print()
-  print('No of partitions - {}'.format(rdd.getNumPartitions()))
-  print('Word count:')
+  print('No of partitions = {}'.format(rdd.getNumPartitions()))
   wordCounts = rdd.countByValue()
   for word, count in wordCounts.items():
       print("{} : {}".format(word, count))
 
   count = rdd.count()
-  print('Total count - {}'.format(count))
+  print('Total count = {}'.format(count))
 
 if __name__ == "__main__":
   a_03_flatamp_parallel()
