30df99a49591ff7f9c915422b685d786309a2f55
Song Li <da_tou_li@yahoo.com>
2020-02-17 18:52:44 -0500

    Play with reduceByKey

--- b_01_apache-spark-excercise/a_02_word-count.py
+++ b_01_apache-spark-excercise/a_02_word-count.py
@@ -8,9 +8,17 @@ if __name__ == "__main__":
     context.setLogLevel('ERROR')
     
     data = ('This', 'is', 'is', 'cool')
-    rdd = context.parallelize(data, 2)
+    rdd = context.parallelize(data, 2).cache()
     
     print('No of partitions - {}'.format(rdd.getNumPartitions()))
     wordCounts = rdd.countByValue()
     for word, count in wordCounts.items():
+        print("{} : {}".format(word, count))
+
+    print('\nReduce by key:')
+    new_rdd = rdd.map(lambda x: (x,1), preservesPartitioning=True)
+    result = new_rdd.reduceByKey(lambda a, b: a + b).collect()
+
+    print(result)
+    for word, count in result:
         print("{} : {}".format(word, count))
