deb7e0496f956f66b4729691a6ec852380351767
Song Li <da_tou_li@yahoo.com>
2020-02-17 18:05:46 -0500

    Played with the dataframe in spark

---
+++ b_01_apache-spark-excercise/a_04_dataframe_basic.py
@@ -0,0 +1,28 @@
+from pyspark import SparkContext, SQLContext, SparkConf
+from pyspark.sql import Row
+
+def run():
+  conf = SparkConf().setAppName("word count") \
+    .setMaster("local[2]")
+
+  context = SparkContext(conf = conf)
+  sqlContext = SQLContext(context)
+  context.setLogLevel('ERROR')
+
+  data = [('Song',25),('Trump',22),('Yong',20),('Obama',26)]
+  rdd = context.parallelize(data, 2).map(lambda x: Row(name=x[0], age=int(x[1])))
+  people = sqlContext.createDataFrame(rdd).cache()
+
+  people.printSchema()
+  old_guy = people.orderBy('age', ascending = False).take(1)
+  print(old_guy)
+
+  same_old_guy = [Row(name=x['name'], age=x['age'], other=1) for x in old_guy]
+  print(same_old_guy)
+
+  total = people.groupBy().sum('age').collect()[0][0]
+  print('Total age is {}'.format(total))
+
+# Run the app
+if __name__ == "__main__":
+  run()
