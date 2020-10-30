b685d566dcb425dcdf8a573e817b1662c6729006
Song Li <da_tou_li@yahoo.com>
2020-10-30 09:57:32 -0400

    Small change

--- d-01-prime-calculation/calculate.py
+++ d-01-prime-calculation/calculate.py
@@ -1,12 +1,18 @@
+# Prime numbers smaller than 1000
 from primes import primes
 
 A = pow(2016, 239) - 1
+result = []
 
 def calculate():
-  for i in primes: 
-    print(i)
+  for p in primes: 
+    if A % p == 0:
+      result.append(p)
 
-  # print(A)
+    if len(result) == 4:
+      break
+
+  print(result)
 
 if __name__ == '__main__':
   calculate()
