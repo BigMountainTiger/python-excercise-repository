0ebfb6cb3c4eed7657cc307af623e4f1ae67a20a
Song Li <da_tou_li@yahoo.com>
2020-11-21 18:23:13 -0500

    Remove not used files

--- c-02-python-word-pdf/cdk.js
+++ c-02-python-word-pdf/cdk.js
@@ -1,9 +1,7 @@
 #!/usr/bin/env node
 
 const cdk = require('@aws-cdk/core');
-const { C02PythonWordPdfEcrStack } = require('./stacks/c-02-python-word-pdf-ecr-stack');
 const { C02PythonWordPdfFargateStack } = require('./stacks/c-02-python-word-pdf-fargate-stack');
 
 const app = new cdk.App();
-// new C02PythonWordPdfEcrStack(app, 'C02-PYTHON-WORD-PDF-ECR-Stack');
 new C02PythonWordPdfFargateStack(app, 'C02-PYTHON-WORD-PDF-FARGATE-Stack');
