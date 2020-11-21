32d5372e471c8884b968891dd183e6356fb41030
Song Li <da_tou_li@yahoo.com>
2020-11-21 17:06:04 -0500

    Pushed to the registry

--- c-02-python-word-pdf/stacks/c-02-python-word-pdf-ecr-stack.js
+++ c-02-python-word-pdf/stacks/c-02-python-word-pdf-ecr-stack.js
@@ -1,9 +1,18 @@
 const cdk = require('@aws-cdk/core');
+const ecr = require('@aws-cdk/aws-ecr');
 
 class C02PythonWordPdfEcrStack extends cdk.Stack {
 
   constructor(scope, id, props) {
     super(scope, id, props);
+
+    const REPOSITORY_ID = `${id}-REPOSITORY`;
+    const REPOSITORY_NAME = `experiment-1`;
+    new ecr.Repository(this, REPOSITORY_ID, {
+      repositoryName: REPOSITORY_NAME,
+      removalPolicy: cdk.RemovalPolicy.DESTROY
+      
+    })
 
   }
 }
