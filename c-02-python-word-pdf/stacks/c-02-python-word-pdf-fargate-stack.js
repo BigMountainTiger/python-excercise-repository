76454d5c8dd0c9e3ad50627473bc391806d905b1
Song Li <da_tou_li@yahoo.com>
2020-11-21 17:22:46 -0500

    Added C02PythonWordPdfFargateStack

---
+++ c-02-python-word-pdf/stacks/c-02-python-word-pdf-fargate-stack.js
@@ -0,0 +1,11 @@
+const cdk = require('@aws-cdk/core');
+
+class C02PythonWordPdfFargateStack extends cdk.Stack {
+
+  constructor(scope, id, props) {
+    super(scope, id, props);
+
+  }
+}
+
+module.exports = { C02PythonWordPdfFargateStack }
