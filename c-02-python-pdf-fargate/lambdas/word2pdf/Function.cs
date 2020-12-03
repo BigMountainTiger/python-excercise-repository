54ea78db510016e8e642cfdee3ed50f5d733134e
Song Li <da_tou_li@yahoo.com>
2020-12-03 18:00:43 -0500

    Keep the code

--- c-02-python-pdf-fargate/lambdas/word2pdf/Function.cs
+++ c-02-python-pdf-fargate/lambdas/word2pdf/Function.cs
@@ -1,9 +1,4 @@
-using System.Collections.Generic;
-using System.Linq;
 using System.Threading.Tasks;
-using System.Net.Http;
-using Newtonsoft.Json;
-
 using Amazon.Lambda.Core;
 
 [assembly: LambdaSerializer(typeof(Amazon.Lambda.Serialization.Json.JsonSerializer))]
