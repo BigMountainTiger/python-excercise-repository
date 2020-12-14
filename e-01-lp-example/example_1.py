f2a674e7baddff02ee548821816386b4a8947abf
Song Li <da_tou_li@yahoo.com>
2020-12-14 11:13:40 -0500

    Completed the first LP example

--- e-01-lp-example/example_1.py
+++ e-01-lp-example/example_1.py
@@ -5,7 +5,29 @@
 # x >= 0
 # y >= 0
 
+import numpy as np
+import scipy.optimize as opt
+
 def example_1():
+
+  cost = np.array([-50, -40])
+
+  A = [np.array([2, 3]), np.array([2, 1])]
+  B = [1500, 1000]
+  C = [(0, None), (0, None)]
+
+  obj_fun = lambda x: np.sum(x*cost)
+  cond = (
+    {'type': 'ineq', 'fun': lambda x: B[0] - np.sum(A[0]*x)},
+    {'type': 'ineq', 'fun': lambda x: B[1] - np.sum(A[1]*x)}
+  )
+
+  guess = [1, 1]
+
+  result = opt.minimize(obj_fun, guess, method = 'SLSQP', bounds = C, constraints = cond)
+
+  print(result)
+  print(f'Result is {(result.x)[0]} - {(result.x)[1]}')
 
   print('Completed')
 
