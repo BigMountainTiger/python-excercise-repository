ec746b7282c8ce25d8e6b09e2f49cd0fcdbeda03
Song Li <da_tou_li@yahoo.com>
2020-12-14 09:53:55 -0500

    Adding the example 1

--- e-01-lp-example/example_1.py
+++ e-01-lp-example/example_1.py
@@ -1,3 +1,10 @@
+# https://www.superprof.co.uk/resources/academic/maths/linear-algebra/linear-programming/linear-programming-examples.html
+# f(x,y)= 50x + 40y
+# 2x + 3y <= 1500
+# 2x + y <= 1000
+# x >= 0
+# y >= 0
+
 def example_1():
 
   print('Completed')
