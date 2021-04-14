6419fbc764152cadae026ae949eaabfe96fcc86b
Song Li <da_tou_li@yahoo.com>
2021-04-14 17:31:11 -0400

    Keep the code

--- pytorch_get_started/2_basics/5_build_model/build_model.py
+++ pytorch_get_started/2_basics/5_build_model/build_model.py
@@ -8,6 +8,8 @@ def build_model():
   device = 'cuda' if torch.cuda.is_available() else 'cpu'
   print('Using {} device'.format(device))
 
+  # https://stackoverflow.com/questions/48619928/unable-to-learn-xor-representation-using-2-layers-of-multi-layered-perceptron-m
+  # https://gist.github.com/RichardKelley/17ef5f2291c273de11540c33dc1bfbf2
   class NeuralNetwork(nn.Module):
     def __init__(self):
       super(NeuralNetwork, self).__init__()
@@ -16,7 +18,7 @@ def build_model():
         nn.ReLU(),
         nn.Linear(3, 3),
         nn.ReLU(),
-        nn.Linear(3, 2),
+        nn.Linear(3, 1),
         nn.ReLU()
       )
 
@@ -26,6 +28,11 @@ def build_model():
 
   model = NeuralNetwork().to(device)
   print(model)
+
+  X = torch.rand(1, 2, device=device)
+  logits = model(X)
+
+  print(logits)
 
 if __name__ == '__main__':
   build_model()
