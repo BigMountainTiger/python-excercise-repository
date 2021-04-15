b7dfa1bff260898843b0781415c6914eeab44a11
Song Li <da_tou_li@yahoo.com>
2021-04-15 10:16:59 -0400

    Added the XOR example

--- pytorch_get_started/2_basics/5_build_model/build_model.py
+++ pytorch_get_started/2_basics/5_build_model/build_model.py
@@ -1,8 +1,11 @@
 import os
 import torch
 from torch import nn
+from torch import optim
+from torch.autograd import Variable
 from torch.utils.data import DataLoader
 from torchvision import datasets, transforms
+import numpy as np
 
 def build_model():
   device = 'cuda' if torch.cuda.is_available() else 'cpu'
@@ -14,10 +17,9 @@ def build_model():
     def __init__(self):
       super(NeuralNetwork, self).__init__()
       self.linear_relu_stack = nn.Sequential(
-        nn.Linear(2, 3),
+        nn.Linear(2, 10),
         nn.ReLU(),
-        nn.Linear(3, 1),
-        nn.ReLU()
+        nn.Linear(10, 1),
       )
 
     def forward(self, x):
@@ -29,8 +31,42 @@ def build_model():
 
   X = torch.rand(1, 2, device=device)
   logits = model(X)
-
   print(logits)
+
+  loss_fn = nn.MSELoss()
+  optimizer = optim.Adam(model.parameters(), lr=1e-3)
+
+  training_epochs = 3000
+
+  pairs = [(np.asarray([0.0,0.0]), [0.0]),
+    (np.asarray([0.0,1.0]), [1.0]),
+    (np.asarray([1.0,0.0]), [1.0]),
+    (np.asarray([1.0,1.0]), [0.0])
+  ]
+
+  state_matrix = np.vstack([x[0] for x in pairs])
+  label_matrix = np.vstack([x[1] for x in pairs])
+
+  for i in range(training_epochs):
+        
+    for batch_ind in range(4):
+      minibatch_state_var = Variable(torch.Tensor(state_matrix))
+      minibatch_label_var = Variable(torch.Tensor(label_matrix))
+              
+      y_pred = model(minibatch_state_var)
+      
+      loss = loss_fn(y_pred, minibatch_label_var)
+      # print(i, batch_ind, loss.data[0])
+
+      optimizer.zero_grad()
+      loss.backward()
+      optimizer.step()
+
+  print('Function after training:')
+  print("f(0,0) = {}".format(model(Variable(torch.Tensor([0.0,0.0]).unsqueeze(0)))))
+  print("f(0,1) = {}".format(model(Variable(torch.Tensor([0.0,1.0]).unsqueeze(0)))))
+  print("f(1,0) = {}".format(model(Variable(torch.Tensor([1.0,0.0]).unsqueeze(0)))))
+  print("f(1,1) = {}".format(model(Variable(torch.Tensor([1.0,1.0]).unsqueeze(0)))))
 
 if __name__ == '__main__':
   build_model()
