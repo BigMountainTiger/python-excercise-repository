156a7bb2b571b3ccfcdb3e8e48735d13bd16052f
Song Li <da_tou_li@yahoo.com>
2021-04-26 18:03:08 -0400

    Keep the code

--- pytorch_torchaudio/silero_speech_to_text_models/run.py
+++ pytorch_torchaudio/silero_speech_to_text_models/run.py
@@ -6,14 +6,8 @@ import torchaudio
 from glob import glob
 
 def run():
-  device = torch.device('cpu')
-
-  model, decoder, utils = torch.hub.load(
-    repo_or_dir='snakers4/silero-models',
-    model='silero_stt',
-    language='en',
-    device=device
-  )
+  device = 'cuda' if torch.cuda.is_available() else 'cpu'
+  model, decoder, utils = torch.hub.load(repo_or_dir = 'snakers4/silero-models', model = 'silero_stt', language = 'en', device = device)
 
   print('Done')
 
