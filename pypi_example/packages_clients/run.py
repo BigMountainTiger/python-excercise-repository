9ee23e3b4b9fd733247ea4a53f910a1263347b23
Song Li <da_tou_li@yahoo.com>
2026-02-10 11:32:15 -0500

    Updated the python-excercise-repository

---
+++ pypi_example/packages_clients/run.py
@@ -0,0 +1,25 @@
+import sys
+
+
+class UserModules:
+    # A simple utility to print user-defined modules loaded in sys.modules
+    _system_modules = set(sys.modules.keys())
+
+    @classmethod
+    def user_modules(cls):
+        return {k: v for k, v in sys.modules.items() if k not in cls._system_modules}
+
+    @classmethod
+    def print_user_modules(cls):
+        print()
+        for k, v in cls.user_modules().items():
+            print(k)
+            print(f'  -> {v.__file__}\n')
+
+
+if __name__ == "__main__":
+
+    import first_example_package
+    UserModules.print_user_modules()
+
+    first_example_package.print_message()
